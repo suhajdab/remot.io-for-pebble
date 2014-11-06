b5277e2823a3e6f398d9bcda16d292387c4fe9f8
Balázs Suhajda <balazs@onereason.eu>
2014-11-06 11:15:31 +0100

    collecting useful apis

--- src/app.js
+++ src/app.js
@@ -2,9 +2,74 @@ var UI = require('ui');
 
 // Create a Card with title and subtitle
 var card = new UI.Card({
-  title:'Weather',
+  title:'remot.io',
   subtitle:'Fetching...'
 });
 
+card.body('This is the content of my card!');
+
 // Display the Card
 card.show();
+
+
+/* for accelerometer controlling presentation
+
+var Accel = require('ui/accel');
+Accel.init();
+Accel.on('tap', function(e) {
+  console.log('Tap event on axis: ' + e.axis + ' and direction: ' + e.direction);
+});
+
+*/
+
+/* ajax to communicate with remot.io server
+var ajax = require('ajax');
+
+ajax(
+  {
+    url: 'http://api.theysaidso.com/qod.json',
+    type: 'json'
+  },
+  function(data) {
+    console.log('Quote of the day is: ' + data.contents.quote);
+  },
+  function(error) {
+    console.log('The ajax request failed: ' + error);
+  }
+);
+*/
+
+/* button event listeners
+
+wind.on('click', 'up', function() {
+  console.log('Up clicked!');
+});
+
+*/
+
+/* splash screen
+
+var splashScreen = new UI.Card({ banner: 'images/splash.png' });
+splashScreen.show();
+
+var mainScreen = new UI.Menu();
+
+setTimeout(function() {
+  // Display the mainScreen
+  mainScreen.show();
+  // Hide the splashScreen to avoid showing it when the user press Back.
+  splashScreen.hide();
+}, 400);
+
+*/
+
+/* action menu - icons next to buttons
+
+var card = new UI.Card({
+  action: {
+    up: 'images/action_icon_plus.png',
+    down: 'images/action_icon_minus.png'
+  }
+});
+
+*/
